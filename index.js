dc3712a4cf93609d3db9d76b03439bf0b9544e8c
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-12-30 17:06:51 +0600

    sign-up

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +17,21 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function poppers () {
 try{
-
+    const userCollection = client.db('Poppers').collection('users');
+    
+    /* const user = {
+        name: 'tabu',
+        email: 'tabu@gmail.com'
+    }
+    const result = await userCollection.insertOne(user)
+    console.log(result); */
+    
+    app.post('/users', async(req, res)=>{
+        const poppersUser = req.body;
+        // console.log(poppersUser);
+        const result = await userCollection.insertOne(poppersUser)
+        res.send(result);
+    })
 }
 finally{
 
